fc1535773c18d9142e938f93f3dad4a5850a77a2
Георгий Князев <saint_georgy@yahoo.com>
2022-06-23 12:21:49 +0300

    Добавьте файлы проекта.

---
+++ 10 uslovniy operator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = 9;
+            int numberForExponentiation = 2;
+            int numberInPower = 1;
+            int degreeOfNumber = 1;
+ 
+            while (randomNumber > numberInPower)
+            {
+                numberInPower *= numberForExponentiation;
+               
+                if (numberInPower > randomNumber)
+                {
+                    Console.WriteLine("Исходное число: " + randomNumber);
+                    Console.WriteLine("Число больше него: " + numberInPower);
+                    Console.WriteLine("Степень исходного числа " + degreeOfNumber);
+                }
+                degreeOfNumber++;
+            }
+        }
+    }
+}
